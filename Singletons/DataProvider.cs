162f0774636cdccec5db9054a58e6cce94b58844
GaoSoiBac <tomnguyenhieu2004@gmail.com>
2025-03-07 01:10:48 +0700

    Add Singleton

---
+++ Singletons/DataProvider.cs
@@ -0,0 +1,31 @@
+﻿using QuanAnNhat.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanAnNhat.Singletons
+{
+    class DataProvider
+    {
+        private static DataProvider _Ins;
+        public static DataProvider Ins
+        {
+            get
+            {
+                if (_Ins is null) _Ins = new DataProvider();
+                return _Ins;
+            }
+            set
+            {
+                _Ins = value;
+            }
+        }
+        public QuanannhatContext Context { get; set; }
+        private DataProvider()
+        {
+            Context = new QuanannhatContext();
+        }
+    }
+}
