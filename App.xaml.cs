c284597017ad1407f05fba92d39499264d23904f
GaoSoiBac <tomnguyenhieu2004@gmail.com>
2025-03-07 01:13:02 +0700

    Add ???

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using QuanAnNhat.Models;
+using QuanAnNhat.Singletons;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace QuanAnNhat;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+        //DataProvider.Ins.Context.Dishes.ToList();
+        //DataProvider.Ins.Context.Dishlists.ToList();
+    }
+}
+
