ff6e0437b7fa8cb9c87c317c7738762862f050ce
GaoSoiBac <tomnguyenhieu2004@gmail.com>
2025-03-07 01:11:45 +0700

    Add MainWindow

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanAnNhat.ViewModels
+{
+    partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableObject _SelectedViewModel;
+
+        public MainViewModel()
+        {
+            //
+        }
+
+        [RelayCommand]
+        public void ExcuteUpdateView(object parameter)
+        {
+            switch (parameter.ToString())
+            {
+                case "Home":
+                    SelectedViewModel = new HomeViewModel();
+                    break;
+                case "Order":
+                    SelectedViewModel = new OrderViewModel();
+                    break;
+            }
+        }
+    }
+}
