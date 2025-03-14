91ebc166167456058f14ae34ed0b39810501c89c
GaoSoiBac <tomnguyenhieu2004@gmail.com>
2025-03-14 22:21:21 +0700

    Remove Connection String

--- DBContext/QuanannhatContext.cs
+++ DBContext/QuanannhatContext.cs
@@ -45,7 +45,6 @@ public partial class QuanannhatContext : DbContext
     public virtual DbSet<Wishlist> Wishlists { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=quanannhat;Persist Security Info=True;User ID=sa;Password=sa;Encrypt=True;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
