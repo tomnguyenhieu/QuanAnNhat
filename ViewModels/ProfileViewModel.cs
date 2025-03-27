f3eb71364255fa463b2dce3d408331539e3ba90c
GaoSoiBac <tomnguyenhieu2004@gmail.com>
2025-03-27 22:14:26 +0700

    Add ChangePassword

--- ViewModels/ProfileViewModel.cs
+++ ViewModels/ProfileViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using MailKit.Net.Smtp;
 using Microsoft.EntityFrameworkCore;
+using MimeKit;
 using QuanAnNhat.DBContext;
 using QuanAnNhat.Models;
 using System;
@@ -9,6 +11,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Windows.System;
 
 namespace QuanAnNhat.ViewModels
 {
@@ -25,6 +28,13 @@ namespace QuanAnNhat.ViewModels
         private Visibility _FeedbackVisible;
         [ObservableProperty]
         private Information _Information;
+        [ObservableProperty]
+        private string? _Email;
+        [ObservableProperty]
+        private string? _NewPassword;
+        [ObservableProperty]
+        private string? _AuthCode;
+        private string? GenerateCode;
 
         public ProfileViewModel(string? userId)
         {
@@ -72,6 +82,78 @@ namespace QuanAnNhat.ViewModels
             Addresses.AddRange(provinces);
         }
 
+        public void HandleSaveChange(string tab)
+        {
+            switch (tab)
+            {
+                case "Information":
+                    if (Information.Phone.Count() != 10)
+                    {
+                        MessageBox.Show("Vui lòng kiểm tra lại số điện thoại!");
+                    }
+                    else
+                    {
+                        using (var context = new QuanannhatContext())
+                        {
+                            var user = context.Users.Where(u => u.Id == _UserId).Include(u => u.Information).First();
+                            context.Informations.Where(i => i.Id == user.InformationId).ExecuteUpdate(u => u.SetProperty(u => u.Name, Information.Name));
+                            context.Informations.Where(i => i.Id == user.InformationId).ExecuteUpdate(u => u.SetProperty(u => u.Gender, Information.Gender));
+                            context.Informations.Where(i => i.Id == user.InformationId).ExecuteUpdate(u => u.SetProperty(u => u.Phone, Information.Phone));
+                            context.Informations.Where(i => i.Id == user.InformationId).ExecuteUpdate(u => u.SetProperty(u => u.Address, Information.Address));
+                            context.SaveChanges();
+                            MessageBox.Show("Cập nhật thông tin thành công!");
+                        }
+                    }
+                    break;
+                case "Password":
+                    if (!string.IsNullOrWhiteSpace(NewPassword) && AuthCode == GenerateCode && NewPassword.Count() >= 8)
+                    {
+                        using (var context = new QuanannhatContext())
+                        {
+                            var user = context.Users.Where(u => u.Id == _UserId).Include(u => u.Information).First();
+                            context.Users.Where(u => u.Id == user.Id).ExecuteUpdate(u => u.SetProperty(u => u.Password, NewPassword));
+                            context.SaveChanges();
+                        }
+                        MessageBox.Show("Cập nhật mật khẩu mới thành công!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Vui lòng kiểm tra lại!");
+                    }
+                    break;
+            }
+        }
+
+        public async void SendGenerateCode()
+        {
+            if (!Email.Contains("@") || Email.Contains(" ") || Email is null)
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!");
+
+            }
+            else
+            {
+                MailboxAddress MailAddressFrom = new MailboxAddress("Sakuramen", "tomnguyenhieu2004@gmail.com");
+                MailboxAddress MailAddressTo = new MailboxAddress("User", $"{Email}");
+                var message = new MimeMessage();
+                message.From.Add(MailAddressFrom);
+                message.To.Add(MailAddressTo);
+                message.Subject = "Bạn có 1 thông báo mới từ Saku Ramen!!!";
+                message.Body = new TextPart("plain")
+                {
+                    Text = $"Mã xác thực của bạn là: {GenerateCode}"
+                };
+                using (var client = new SmtpClient())
+                {
+                    client.Connect("smtp.gmail.com", 587, false);
+                    client.Authenticate("tomnguyenhieu2004@gmail.com", "hryd ptlm ryey rgim");
+                    client.Send(message);
+                    client.Disconnect(true);
+                }
+                MessageBox.Show("Vui lòng kiểm tra Gmail!");
+            }
+        }
+
         [RelayCommand]
         public void ExecuteChangeTab(object parameter)
         {
@@ -96,27 +178,20 @@ namespace QuanAnNhat.ViewModels
         }
 
         [RelayCommand]
-        public void ExecuteSaveChanges()
+        public void ExecuteSaveChanges(object parameter)
         {
-            if (Information.Phone.Count() != 10)
-            {
-                MessageBox.Show("Vui lòng kiểm tra lại số điện thoại!");
-            } else
-            {
-                using (var context = new QuanannhatContext())
-                {
-                    var user = context.Users.Where(u => u.Id == _UserId).Include(u => u.Information).First();
+            HandleSaveChange(parameter.ToString());
+        }
 
-                    context.Informations.Where(i => i.Id == user.InformationId).ExecuteUpdate(u => u.SetProperty(u => u.Name, Information.Name));
-                    context.Informations.Where(i => i.Id == user.InformationId).ExecuteUpdate(u => u.SetProperty(u => u.Gender, Information.Gender));
-                    context.Informations.Where(i => i.Id == user.InformationId).ExecuteUpdate(u => u.SetProperty(u => u.Phone, Information.Phone));
-                    context.Informations.Where(i => i.Id == user.InformationId).ExecuteUpdate(u => u.SetProperty(u => u.Address, Information.Address));
+        [RelayCommand]
+        public async Task ExecuteSendCode()
+        {
+            Random random = new Random();
+            GenerateCode = $"{random.Next(9)}{random.Next(9)}{random.Next(9)}{random.Next(9)}";
 
-                    context.SaveChanges();
+            Console.WriteLine(GenerateCode);
 
-                    MessageBox.Show("Cập nhật thông tin thành công!");
-                }
-            }
+            SendGenerateCode();
         }
     }
 }
